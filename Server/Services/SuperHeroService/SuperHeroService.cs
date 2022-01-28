c021951b9e08f2bad13325516624dfa84a55049b
grandaddy87 <campbell0807@live.com>
2022-01-28 00:38:25 -0500

    Add project files.

---
+++ Server/Services/SuperHeroService/SuperHeroService.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorWalkingSkeleton.Server.Services.SuperHeroService
+{
+    public class SuperHeroService : ISuperHeroService
+    {
+        private readonly DataContext _context;
+
+        public SuperHeroService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SuperHero>> GetAll()
+        {
+            var heroes = await _context.SuperHeroes.ToListAsync();
+            return heroes;
+        }
+    }
+}
